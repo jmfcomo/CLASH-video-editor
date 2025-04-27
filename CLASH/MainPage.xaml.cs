b669236f69a5cbf7cd7133a83289f44506a734dc
Joan Ferguson <43357248+jmfcomo@users.noreply.github.com>
2025-04-27 11:04:22 -0500

    basic uwp app

---
+++ CLASH/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CLASH
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            // Load the ClipView on initial startup
+            ContentFrame.Navigate(typeof(ClipView));
+        }
+
+        private void ClipViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(ClipView));
+        }
+
+        private void SequenceViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(SequenceView));
+        }
+    }
+}
